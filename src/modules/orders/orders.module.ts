9bd04fba61d55c2e244f9a56237d79a45b78d013
tikken <big_nick@gmail.com>
2020-02-09 21:49:31 +0300

    typeorm repository module

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,8 +1,13 @@
 import {Module} from '@nestjs/common';
 import {OrdersController} from '../../controllers/orders/orders.controller';
 import {OrdersService} from '../../services/orders/orders.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {OrderRepository} from "../../repositories/order.repository";
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([OrderRepository])
+    ],
     controllers: [OrdersController],
     providers: [OrdersService]
 })
