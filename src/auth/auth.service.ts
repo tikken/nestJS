e373fadeee35876f3f0e832ac143d04d86fde838
tikken <big_nick@gmail.com>
2020-02-11 07:16:27 +0300

    signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,5 +10,10 @@ export class AuthService {
     private userRepository: UserRepository
   ) {
 
+  }
+
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+      return this.userRepository.signUp(authCredentialsDto);
   }
 }
