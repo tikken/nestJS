98eadb99420825c05f6e9aab8ffab7a20371fa1d
tikken <big_nick@gmail.com>
2020-02-11 10:02:36 +0300

    orders guards

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,52 +10,55 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
-import {OrdersService} from './orders.service';
-import {CreateOrderDto} from './create-order.dto';
-import {GetOrderFilterDto} from "./get-order.dto";
-import {OrderStatusValidationPipe} from "./order-status-validation.pipe";
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './create-order.dto';
+import { GetOrderFilterDto } from './get-order.dto';
+import { OrderStatusValidationPipe } from './order-status-validation.pipe';
 import { Order } from './order.entity';
 import { OrderStatus } from './order.model';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('orders')
+@UseGuards(AuthGuard())
 export class OrdersController {
-    constructor(private ordersService: OrdersService) {
-    }
+  constructor(private ordersService: OrdersService) {
+  }
 
-    //http://localhost:3000/orders?status=OPEN&search=4m
-    @Get()
-    getOrders(@Query(ValidationPipe) filterDto: GetOrderFilterDto): Promise<Order[]> {
-        return this.ordersService.getOrders(filterDto);
-    }
+  //http://localhost:3000/orders?status=OPEN&search=4m
+  @Get()
+  getOrders(@Query(ValidationPipe) filterDto: GetOrderFilterDto): Promise<Order[]> {
+    return this.ordersService.getOrders(filterDto);
+  }
 
-    @Get("/:id")
-    getOrderById(@Param('id', ParseIntPipe) id: number): Promise<Order> {
-        return this.ordersService.getOrderById(id);
-    }
+  @Get('/:id')
+  getOrderById(@Param('id', ParseIntPipe) id: number): Promise<Order> {
+    return this.ordersService.getOrderById(id);
+  }
 
-    //ex http://localhost:3000/orders/542f71c0-4b4a-11ea-bb05-ff95b8fcea8d/status
-    @Patch("/:id/status")
-    updateOrderStatus(
-        @Param('id') id: number,
-        @Body('status', OrderStatusValidationPipe) status: OrderStatus
-    ): Promise<Order> {
-        return this.ordersService.updateOrderStatus(id, status);
-    }
+  //ex http://localhost:3000/orders/542f71c0-4b4a-11ea-bb05-ff95b8fcea8d/status
+  @Patch('/:id/status')
+  updateOrderStatus(
+    @Param('id') id: number,
+    @Body('status', OrderStatusValidationPipe) status: OrderStatus,
+  ): Promise<Order> {
+    return this.ordersService.updateOrderStatus(id, status);
+  }
 
-    @Delete("/:id")
-    deleteOrder(@Param('id', ParseIntPipe) id: number): Promise<void> {
-       return this.ordersService.deleteOrder(id);
-    }
+  @Delete('/:id')
+  deleteOrder(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.ordersService.deleteOrder(id);
+  }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createOrder(
-        //stink way
-        // @Body('title') title: string,
-        // @Body('description') description: string
-        @Body() createOrderDto: CreateOrderDto
-    ): Promise<Order> {
-        return this.ordersService.createOrder(createOrderDto);
-    }
+  @Post()
+  @UsePipes(ValidationPipe)
+  createOrder(
+    //stink way
+    // @Body('title') title: string,
+    // @Body('description') description: string
+    @Body() createOrderDto: CreateOrderDto,
+  ): Promise<Order> {
+    return this.ordersService.createOrder(createOrderDto);
+  }
 }
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,10 +3,12 @@ import {OrdersController} from './orders.controller';
 import {OrdersService} from './orders.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {OrderRepository} from "./order.repository";
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([OrderRepository])
+        TypeOrmModule.forFeature([OrderRepository]),
+        AuthModule
     ],
     controllers: [OrdersController],
     providers: [OrdersService]
