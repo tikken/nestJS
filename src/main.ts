338ce8fdb5fe0ca9a619460e55b9fdb0276a9e47
tikken <big_nick@gmail.com>
2020-02-12 22:56:25 +0300

    config files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from "config";
+
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
+  const serverConfig = config.get('server');
 
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+
+  const port = process.env.PORT || serverConfig.port;
 
   await app.listen(port);
 
